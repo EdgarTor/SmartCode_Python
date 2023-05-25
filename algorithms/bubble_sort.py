fba835a5818188397009790338e3b3331bdb5c0e
EdgarTorosian <98470353+EdgarTorosian@users.noreply.github.com>
2023-05-25 13:38:26 +0400

    modified bubble sorting

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,10 +1,9 @@
-arr = [5, 1, 4, 2, 8]
+arr = [5, 4, 8, 2, 1]
 
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        j = i
-        for j in range(n - 1):
+        for j in range(n - i -1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
