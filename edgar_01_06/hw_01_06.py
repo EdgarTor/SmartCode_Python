bac0e11b6fede1b887edf0b0e8bf2600de9fdd12
EdgarTorosian <98470353+EdgarTorosian@users.noreply.github.com>
2023-06-03 01:31:36 +0400

    hw_01_06

---
+++ edgar_01_06/hw_01_06.py
@@ -0,0 +1,54 @@
+'''
+1․ Գրել Triangle class, որը․
+   - __init__() -ում կընդունի եռանկյան կողմերը և կստուգի արդյոք նման կողմերով 
+   եռանկյուն գոյություն ունի թե ոչ,
+     եթե կողմերը սխալ են տրված կվերադարձնի Error համապատասխան տեքստով,
+   - կունենա մեթոդ, որը կվերադարձնի եռանկյան կողմերը,
+   - կունենա մեթոդ, որը կվերադարձնի եռանկյան պարագիծը,
+   - կունենա մեթոդ, որը կվերադարձնի եռանկյան մակերեսը,
+   - կունենա մեթոդ, որը կվերադարձնի արդյոք եռանկյունը հավասարակողմ է, 
+   հավասարասրուն, թե անկանոն (կողմերը իրար = չեն)։
+'''
+
+class Triangle:
+    def __init__(self, side_1: int, side_2: int, side_3: int):
+        if side_1 + side_2 > side_3 and side_1 + side_3 > side_2 \
+            and  side_2 + side_3 > side_1:
+
+            self.side_1 = side_1
+            self.side_2 = side_2
+            self.side_3 = side_3
+        else:
+            raise Exception("Can not create a triangle with provided values")
+        
+    def get_sides(self) -> str:
+        return f"side 1: {self.side_1}, side 2: {self.side_2}, " + \
+                f"side 3: {self.side_3}"
+    
+    def get_perimeter(self) -> int:
+        return self.side_1 + self.side_2 + self.side_3
+    
+    def get_area(self) -> float:
+        s = self.get_perimeter() / 2
+        area = (s * (s - self.side_1) * (s - self.side_2) * (s - self.side_3)) ** 0.5
+        
+        return round(area, 1)   
+
+    def get_type(self) -> str:
+        if self.side_1 == self.side_2 and self.side_2 == self.side_3:
+            return "Equilateral triangle"
+        elif self.side_1 == self.side_2 or self.side_2 == self.side_3 \
+                or self.side_1 == self.side_3:
+            return "Isosceles triangle"
+        else:
+            return "Scalene triangle"
+
+        
+t = Triangle(7, 8, 9)
+print(t.get_sides())
+print(t.get_perimeter())
+print(t.get_area())
+print(t.get_type())
+
+
+    
